75a9bcaac5ac7f88ca5c3a42f625ff22ecf2b865
Yeddu Naresh Kumar <nareshy@VALUEMOMENTUM.BIZ>
2019-07-30 17:08:02 +0530

    files

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$keyId = 'rzp_test_4i3LefSg9jl9Ul';
+$keySecret = 'cQB9faZELhLGT7eiraCmZU6g';
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
